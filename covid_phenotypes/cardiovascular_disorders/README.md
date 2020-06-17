19f5ec80330efc8533d2ca60a4ea597a7340ab28
Michael Zietz <michael.zietz@gmail.com>
2020-06-16 19:04:58 -0500

    Add cardiovascular and respiratory disorders (#12)

---
+++ covid_phenotypes/cardiovascular_disorders/README.md
@@ -0,0 +1,12 @@
+## Cardiovascular disorders
+
+Broad category encompassing a large number of disorders.
+Defined using diagnosis codes only.
+
+Using descendants of [SNOMED 49601007 (Disorder of cardiovascular system)](https://athena.ohdsi.org/search-terms/terms/134057) with standard to non-standard mapping to ICD-10 CM.
+
+<!---
+```SQL
+{}
+```
+-->
---
+++ covid_phenotypes/respiratory_disorders/phenotype.sql
@@ -0,0 +1,21 @@
+SELECT DISTINCT pat_mrn_id
+FROM (
+    -- From COVID table (descendants of OMOP 320136 mapped to ICD-10 CM)
+    SELECT DISTINCT pat_mrn_id
+    FROM concept_ancestor
+    INNER JOIN concept_relationship ON descendant_concept_id = concept_id_1
+    INNER JOIN concept ON concept_id_2 = concept_id
+    INNER JOIN 1_covid_patients_noname ON concept_code = REPLACE(icd10_code, ",", "")
+    WHERE ancestor_concept_id = 320136 AND
+        relationship_id IN ("Included in map from", "Mapped from") AND
+        vocabulary_id = "ICD10CM" AND date_retrieved = @date
+
+    UNION ALL
+
+    -- From OMOP table (descendants of OMOP 320136)
+    SELECT DISTINCT pat_mrn_id
+    FROM concept_ancestor
+    INNER JOIN condition_occurrence ON descendant_concept_id = condition_concept_id
+    INNER JOIN 1_covid_patient2person using (person_id)
+    WHERE ancestor_concept_id = 320136
+) AS respiratory_disease_patients
